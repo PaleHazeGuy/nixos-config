a7cba0a8e6a6292418478f92fc18291da7ecaad3
Sirisart <alternativelegobrick@gmail.com>
2026-07-29 11:15:57 +0700

    feat(main-user/rog-flow-z13): enable nautilus

--- home/main-user/rog-flow-z13.nix
+++ home/main-user/rog-flow-z13.nix
@@ -6,8 +6,12 @@
   userSettings = {
     programs = {
       neovim.enable = true;
+
       yazi.enable = true;
+      nautilus.enable = true;
+
       kitty.enable = true;
+      
       chromium.enable = true;
 
       hyprland.enable = true;
