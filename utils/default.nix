702866e1aa8297de802da32b360e86fb578b076b
Sirisart <alternativelegobrick@gmail.com>
2026-07-31 04:45:08 +0700

    feat(utils): add mkDotfiles helper for folder-to-home.file mapping

--- utils/default.nix
+++ utils/default.nix
@@ -5,4 +5,9 @@
     (lib.concatStringsSep "\n"
       (lib.mapAttrsToList (name: value: "  ${name} = \"${value}\",") attrs))
     + "\n}";
+
+  mkDotfiles = { dir, prefix }:
+		lib.mapAttrs' (name: _:
+			lib.nameValuePair "${prefix}/${name}" { source = dir + "/${name}"; }
+		) (builtins.readDir dir);
 }
